0ced9d10a016bc7257813efd65aafcf63d075341
saynschuman <saynschuman@gmail.com>
2017-04-10 13:11:22 +0300

    bugs finished

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,35 @@
 (function($) {
-    
 
+	// sliders
+
+	$('#sl-outer').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: false,
+	  asNavFor: '#sl-inner',
+	  fade: true
+	});
+
+	$('#sl-inner').slick({
+	  slidesToShow: 6,
+	  slidesToScroll: 1,
+	  asNavFor: '#sl-outer',
+	  focusOnSelect: true
+	});	
+
+	$('#sl-mid-outer').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: false,
+	  asNavFor: '#sl-mid-inner',
+	  fade: true
+	});
+
+	$('#sl-mid-inner').slick({
+	  slidesToShow: 4,
+	  slidesToScroll: 1,
+	  asNavFor: '#sl-mid-outer',
+	  focusOnSelect: true
+	});		
 
 })(jQuery);
